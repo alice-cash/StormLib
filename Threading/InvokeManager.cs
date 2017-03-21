e5fbaaa337d0eafd025d0b382e260f6530031a82
Matthew Cash <matthew@stormstudios.org>
2017-03-21 00:06:41 -0500

    Initil Commit

---
+++ Threading/InvokeManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashLib.Threading
+{
+    class InvokeManager: Invoker
+    {
+        private List<Invoker> _invokers;
+
+
+        /// <summary>
+        /// Performs a Synchronous call to the target Manager to transfer an Invoker. This blocks until the recieving thread can accept it.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="item"></param>
+        [ThreadSafe(ThreadSafeFlags.ThreadSafeAsynchronous)]
+        public void GiveInvokerTo(InvokeManager target, Invoker item)
+        {
+            //If an invoke is not required for the target then it is running on the same thread as us.
+            if(target.InvokeRequired())
+                this.InvokeMethod(() => { SynchronousGiveInvokerTo(target, item); });
+            else
+            {
+                //We can just reassign it since the thread runs both objects.
+                target._invokers.Add(item);
+                this._invokers.Remove(item);
+            }
+        }
+
+
+        [ThreadSafe(ThreadSafeFlags.ThreadSafeSynchronous)]
+        private void SynchronousGiveInvokerTo(InvokeManager target, Invoker item)
+        {
+            if (_invokers.Contains(item))
+                target.SynchronousInvokeMethod(() => 
+                {
+                    target._invokers.Add(item);
+                    this._invokers.Remove(item);
+                    item.ChangeThreadOwner();
+                });
+            else
+                throw new InvalidOperationException(string.Format("Invoker {0} is not owned by {1}.", item.ToString(), this.ToString()));
+        }
+
+        [ThreadSafe(ThreadSafeFlags.ThreadUnsafe)]
+        internal override void ChangeThreadOwner()
+        {
+            foreach (Invoker item in _invokers)
+                item.ChangeThreadOwner();
+            base.ChangeThreadOwner();
+        }
+    }
+}
---
+++ Threading/ThreadSafeFlags.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashLib.Threading
+{
+    /// <summary>
+    /// Flag state for the ThreadSafeAttribute class.
+    /// </summary>
+    [Flags]
+    public enum ThreadSafeFlags
+    {
+        /// <summary>
+        /// Function is thread safe and may or may not block calls.
+        /// </summary>
+        ThreadSafe,
+        /// <summary>
+        /// Function is not thread safe and will throw an exception if 
+        /// called in an unsafe manor.
+        /// </summary>
+        ThreadSafeEnforced,
+        /// <summary>
+        /// Function is not thread safe and may have undefined behavior
+        /// when called in an unsafe manor.
+        /// </summary>
+        ThreadUnsafe,
+        /// <summary>
+        /// Function is thread safe and Asynchronous, calls will not 
+        /// block but may not run immediatly.
+        /// </summary>
+        ThreadSafeAsynchronous,
+        /// <summary>
+        /// Function is thread safe and is Synchronous, calls will block
+        /// until the parent thread runs if called in an unsafe manor.
+        /// </summary>
+        ThreadSafeSynchronous
+    }
+}
